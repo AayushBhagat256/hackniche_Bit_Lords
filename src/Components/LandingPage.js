faec109961b0af3a42380ba9ca9fd29891e6dfae
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-02-17 10:46:21 +0530

    basicLandingsetUp

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BrandExample from './NavLand'
+
+function LandingPage() {
+  return (
+    <div>
+      <BrandExample/>
+    </div>
+  )
+}
+
+export default LandingPage
