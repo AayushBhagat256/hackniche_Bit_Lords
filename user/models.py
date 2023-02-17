b30574b4547b27fd73d673d15b68fbbbf948b4c9
Dev Patel <devpatel0732@gmail.com>
2023-02-17 20:16:23 +0530

    Debug

--- user/models.py
+++ user/models.py
@@ -19,7 +19,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     ]
 
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True,error_messages={'unique': "A user with that email already exists.",})
-    username = models.CharField(('username'), max_length=150, unique=True,error_messages={'unique': "A user with that username already exists.",} )
+    username = models.CharField( max_length=150, unique=True,error_messages={'unique': "A user with that username already exists.",} )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
