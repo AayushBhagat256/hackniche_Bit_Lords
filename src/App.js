5b4423c8a146580751eaa29f1c37cfce3cd44735
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2023-02-17 16:56:22 +0530

    Navbardone

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ import LandingPage from './Components/LandingPage';
 
 function App() {
   return (
-    // <Routes>
-    // <Route path='/' element={<Signin/>}/>
-    // <Route path='/Signin' element={<Signin/>}/>
-    // <Route path='/Signup' element={<Signup/>}/>
-    // </Routes>
-    <LandingPage/>
+    <Routes>
+    <Route path='/' element={<LandingPage/>}/>
+    <Route path='/Signin' element={<Signin/>}/>
+    <Route path='/Signup' element={<Signup/>}/>
+    </Routes>
+    
   );
 }
 
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,89 +1,309 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
+import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import HomeIcon from '@mui/icons-material/Home';
+import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+import ConstructionOutlinedIcon from '@mui/icons-material/ConstructionOutlined';
+// import Home from './Home';
+import InfoIcon from '@mui/icons-material/Info';
+// import AboutMe from './AboutMe';
+// import Form from './Form';
+// import Project from './Project';
+// import Skill from './Skill';
+import { Button } from '@mui/material';
+import Signin from './Signin';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
   [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: `calc(${theme.spacing(8)} + 1px)`,
   },
-}));
+});
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
+const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
 }));
 
-export default function SearchAppBar() {
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function MiniDrawer() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [menuData,setMenuData]=React.useState("Home")
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      {/* elevation used to adjust shadow */}
+      <AppBar position="fixed" sx={{backgroundColor:"#fefbe9",color:"black"}} elevation={3}>
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{ mr: 2 }}
+            // onClick={handleDrawerOpen}
+            onClick={()=>{setOpen(!open)}}
+            edge="start"
+            // sx={{
+            //   marginRight: 5,
+            //   ...(open && { display: 'none' }),
+            // }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            MUI
+          <Typography variant="h6" noWrap component="div" >
+            {/* use img scr height 40 for images of logo */}
+            Valor Link
           </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
+          {/* <Button sx={{flexGrow:1}}>Login</Button> */}
         </Toolbar>
       </AppBar>
+      <Drawer variant="permanent" open={open} PaperProps={{
+    sx: {
+      backgroundColor:"#fefbe9"
+    }
+  }}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          
+            <ListItem  disablePadding sx={{ display: 'block' }} onClick={()=>{setMenuData("Home")}}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                    color:"black"
+                  }}
+                >
+                   {/* <MailIcon /> */}
+                   <HomeIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Home"  sx={{ opacity: open ? 1 : 0 }}  />
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem  disablePadding sx={{ display: 'block' }} onClick={()=>{setMenuData("About Me")}}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                    color:"black"
+                  }}
+                >
+                   {/* <MailIcon /> */}
+                   <InfoIcon/>
+                </ListItemIcon>
+                <ListItemText primary="About Me" sx={{ opacity: open ? 1 : 0 }}  />
+              </ListItemButton>
+            </ListItem>
+         
+        </List>
+        <Divider/>
+        <List>
+        <ListItem  disablePadding sx={{ display: 'block' }} onClick={()=>{setMenuData("Project")}}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                    color:"black"
+                  }}
+                >
+                   {/* <MailIcon /> */}
+                   <ConstructionOutlinedIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Sec 1" sx={{ opacity: open ? 1 : 0 }}  />
+              </ListItemButton>
+            </ListItem>
+            <ListItem  disablePadding sx={{ display: 'block' }} onClick={()=>{setMenuData("Skill")}}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                    color:"black"
+                  }}
+                >
+                   {/* <MailIcon /> */}
+                   <WorkspacePremiumIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Sec 2" sx={{ opacity: open ? 1 : 0 }}  />
+              </ListItemButton>
+            </ListItem>
+        </List>
+        <Divider />
+        <List>
+        <ListItem  disablePadding sx={{ display: 'block' }} onClick={()=>{setMenuData("Contact Me")}}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                    color:"black"
+                  }}
+                >
+                   {/* <MailIcon /> */}
+                   <ConnectWithoutContactIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Contact Me" sx={{ opacity: open ? 1 : 0 }}  />
+              </ListItemButton>
+            </ListItem>
+        </List>
+        {/* <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List> */}
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 ,backgroundColor:"#fefbe9"}}>
+        <DrawerHeader />
+        {menuData==="Home" && <Signin/>}
+        {/* {menuData==="About Me" && <AboutMe/>}
+        {menuData==="Contact Me" && <Form/>}
+        {menuData==="Project" && <Project/>}
+        {menuData==="Skill" && <Skill/>} */}
+        {/* <br /> */}
+        {/* <Form/> */}
+      </Box>
     </Box>
+    </>
   );
 }
