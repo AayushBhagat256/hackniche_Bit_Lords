116aba52716e2cc582b04bbf799d3cdd1f4188b8
Dev Patel <devpatel0732@gmail.com>
2023-02-17 19:50:58 +0530

    Debug

--- user/admin.py
+++ user/admin.py
@@ -8,9 +8,9 @@ from .models import UserProfile
 
 class CustomUserAdmin(UserAdmin):
     model = UserProfile
-    list_display = ('email','username','is_superuser','is_staff','is_active','soldier','Profilepic','fname')
+    list_display = ('email','username','is_superuser','is_staff','is_active')
     list_filter = ('is_staff','soldier')
-    search_fields = ('email','username','soldier')
+    search_fields = ('email','username')
     ordering = ('email','username')
 
 UserProfile = get_user_model()
